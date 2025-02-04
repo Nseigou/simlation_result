4662b9a269e7d0523c5c47fbe7b1694c58378d9d
Nseigou <nagano_1si@yahoo.co.jp>
2025-02-05 05:25:58 +0900

    0205 5:25

--- plot_approximate_curve.py
+++ plot_approximate_curve.py
@@ -4,20 +4,31 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
 # データの設定
-protocols = ["No Authentication", "DSA", "ECDSA", "Ed25519"]
-x_values = np.array([0, 37, 74, 112, 148, 185])  # 各データポイントのインデックス
+protocols = ["No Authentication", "DSA", "ECDSA", "EdDSA"]
+# x_values = np.array([0, 37, 74, 112, 148, 185])  # 各データポイントのインデックス
+x_values = np.array([0, 37, 74, 112, 148, 185])
 y_values = np.array([
-    [0, 0, 0, 0], 
-    [14.1802, 69.9882, 55.0108, 24.4069],
-    [50.5984, 267.433, 194.408, 89.2728],
-    [110.405, 625.504, 436.926, 200.901],
-    [196.971, 1172.57, 910.373, 431.346],
-    [345.059, 1908.7, 1324.55, 635.155]
+    # [0, 0, 0, 0], 
+    # [14.1802, 69.9882, 55.0108, 24.4069],
+    # [50.5984, 267.433, 194.408, 89.2728],
+    # [110.405, 625.504, 436.926, 200.901],
+    # [196.971, 1172.57, 910.373, 431.346],
+    # [345.059, 1908.7, 1324.55, 635.155]
+    [68000, 68000, 68000, 68000],
+    [70939.4, 72023.3, 93749.3, 80647.1],
+    [74089.7, 74989.3, 151853, 106442],
+    [77070.1, 78134.7, 254247, 151401],
+    [80749.9, 81752.5, 409965, 219102],
+    [84548.1, 85541.4, 560753, 285191]
 ])
 
 # 近似曲線の関数（2次関数）
 def quadratic(x, a, b, c):
     return a * x**2 + b * x + c
+
+# 近似曲線の関数（1次関数）
+def linear(x, a, b):
+    return a * x + b
 
 # プロットの設定
 plt.figure(figsize=(9, 6))
@@ -27,20 +38,20 @@ for i, protocol in enumerate(protocols):
     y = y_values[:, i]
 
     # 2次関数のフィッティング
-    params, _ = curve_fit(quadratic, x_values, y)
+    params, _ = curve_fit(quadratic, x_values, y) #ここいじる
     x_fit_extended = np.linspace(min(x_values), max(x_values) + 20, 100)
-    y_fit_extended = quadratic(x_fit_extended, *params) # 拡張した x 範囲で計算
+    y_fit_extended = quadratic(x_fit_extended, *params) #ここいじる
 
     # データポイントと近似曲線をプロット
     plt.scatter(x_values, y, label=f"{protocol} (data)", color=colors[i])
     plt.plot(x_fit_extended, y_fit_extended, color=colors[i], label=f"{protocol} (fit)")
 
 # 軸ラベルとタイトル
-
+plt.xlabel("Number of Nodes", fontsize=14)
 plt.ylabel("Simulation Time [s]", fontsize=14)
 plt.legend()
 plt.grid(axis='y')
-plt.yticks(np.arange(0, np.max(y_values) + 400, 200))  # y軸のグリッドを200ごとに設定
+plt.yticks(np.arange(0, np.max(y_values) + 40000, 100000))  # y軸のグリッドを200ごとに設定
 
 # グラフを表示
 plt.show()
--- plot_bar_graph.py
+++ plot_bar_graph.py
@@ -2,13 +2,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 項目
-categories = ['No Authentication', 'DSA', 'ECDSA', 'Ed25519']
+categories = ['No Authentication', 'DSA', 'ECDSA', 'EdDSA']
 
 # 各データの平均値、最大値、最小値
 # include_liar simTime=250 Throughput
-mean_values = [4.18347, 7.64043, 7.50869, 7.32539]  # 平均値
-max_values = [4.44273, 7.9406, 7.9407, 7.66063]   # 最大値
-min_values = [4.0228, 6.95432, 6.61118, 6.61123]    # 最小値
+# mean_values = [4.18347, 7.64043, 7.50869, 7.32539]  # 平均値
+# max_values = [4.44273, 7.9406, 7.9407, 7.66063]   # 最大値
+# min_values = [4.0228, 6.95432, 6.61118, 6.61123]    # 最小値
 # include_liar simTime=250 PDR
 # mean_values = [48.2002, 88.1545, 86.6152, 84.48212]  # 平均値
 # max_values = [51.2097, 91.6224, 91.6224, 88.34120]   # 最大値
@@ -20,15 +20,20 @@ min_values = [4.0228, 6.95432, 6.61118, 6.61123]    # 最小値
 # simTime=300 Overhead
 # mean_values = [2694.3, 5982.8, 5384.2, 5364.5]  # 平均値
 # max_values = [2777.1, 6054.1, 5557.6, 5411.5]   # 最大値
-# min_values = [2608.4, 5933.4, 5318.6, 5315.4]    # 最小値
-# simTime=300 Overhead
-# mean_values = [2694.3, 5982.8, 5384.2, 5364.5]  # 平均値
-# max_values = [2777.1, 6054.1, 5557.6, 5411.5]   # 最大値
 # min_values = [2608.4, 5933.4, 5318.6, 5315.4]
 # simTime=300 Delay
 # mean_values = [7.02477, 6.07848, 4.568, 5.78833]
 # max_values = [19.7153, 25.3697, 22.5077, 17.7234]   # 最大値
 # min_values = [2.24874, 2.0424, 2.09385, 2.23783]
+# simTime=300 PDR node=185
+# mean_values = [83.2086, 87.801, 86.8936, 87.3915]
+# max_values = [85.5705, 90.2685, 91.6107, 89.2617]   # 最大値
+# min_values = [81.5436, 84.8485, 83.557, 86.2416]
+# simTime=300 memory
+mean_values = [73742.9, 74832.9, 151699, 106226]
+max_values = [74316, 75216, 152160, 106656]   # 最大値
+min_values = [73324, 74360, 151144, 105680]
+
 
 
 # エラーバー（平均値から最大・最小への範囲）
@@ -44,15 +49,18 @@ ax.set_axisbelow(True)
 ax.grid(axis='y', linestyle='--', alpha=1)
 
 # ラベル設定
-ax.set_ylabel("Throughput [kbps]", fontsize=14) # Average packet delively rate [%]
+# Throughput [kbps]
+# Average packet delively rate [%]
+# Overhead [KB]
+ax.set_ylabel("# memory usage [KB]", fontsize=14) 
 ax.set_xticklabels(categories, fontsize=12)
-ax.set_ylim(0, max(max_values) + 2)  # y軸の範囲調整
+ax.set_ylim(0, max(max_values) + 20000)  # y軸の範囲調整 # 2 1000 20000
 # ax.set_ylim(0, 100)  # y軸の範囲調整
 # 各棒の下に平均値を表示
 for bar, mean in zip(bars, mean_values):
     x = bar.get_x() + bar.get_width() / 2  # 棒の中央位置
-    y = -0.7  # 軸の下に配置するためのオフセット                    # 棒の下の少し空いた位置
-    ax.text(x, y, f"{mean:.1f} kbps", ha='center', va='top', fontsize=12, color='black')
+    y = -10000  # 軸の下に配置するためのオフセット    # 0.8 7  500   10000         # 棒の下の少し空いた位置
+    ax.text(x, y, f"{mean:.1f} KB", ha='center', va='top', fontsize=12, color='black')
 
 # グラフを表示
 plt.show()
